c931caea6299cb69f3f1ba90ea62d047fe03ba71
Naskovski <filip.naskovski.hk@gmail.com>
2023-11-11 13:50:22 +0100

    CoordinatesFilter added

---
+++ Домашно 1/PipeAndFilterData/src/pipeandfilter/Impl/CoordinatesFilter.java
@@ -0,0 +1,36 @@
+package pipeandfilter.Impl;
+
+import pipeandfilter.Filter;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CoordinatesFilter implements Filter<String> {
+
+
+    @Override
+    public String execute(String inputFile) {
+
+        //inputCoordinates = "POINT (22.233366 41.4140987)"
+        //outputCoordinates = "22.233366,41.4140987"
+        Pattern pattern = Pattern.compile("POINT \\(([^)]+)");
+
+        String[] modifiedData = Arrays.stream(inputFile.split("/n")).map(row -> {
+            String[] data = row.split(",");
+
+            Matcher matcher = pattern.matcher(data[0]);
+
+            if (matcher.find()) {
+                String[] coordinates = matcher.group(1).split(" ");
+                data[0] = coordinates[0] + "," + coordinates[1];
+            } else {
+                data[0] = "NoInfo,NoInfo";
+            }
+            return String.join(",", data);
+        }).toArray(String[]::new);
+
+
+        return String.join("\n", modifiedData);
+    }
+}
