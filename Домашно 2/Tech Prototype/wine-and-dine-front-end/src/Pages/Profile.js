a78532f86b858bd3acea7437358be744eba49409
Naskovski <filip.naskovski.hk@gmail.com>
2023-12-03 19:57:49 +0100

    Add front-end React App

---
+++ Домашно 2/Tech Prototype/wine-and-dine-front-end/src/Pages/Profile.js
@@ -0,0 +1,23 @@
+import './Profile.css';
+import {Navigate} from "react-router-dom";
+import {useContext} from "react";
+import {AuthContext} from "../AuthContext";
+
+
+const Profile = () => {
+  const {user} = useContext(AuthContext)
+
+
+  return (
+    <div>
+      {user===null?<Navigate to={"/login"}/>:
+          <div>
+            Profil
+          </div>
+      }
+
+    </div>
+  );
+}
+
+export default Profile;
